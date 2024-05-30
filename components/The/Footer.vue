078a395dac843575e6f0dd1c637df81ad91ee484
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-30 20:15:12 +0330

    Update Footer.vue

--- components/The/Footer.vue
+++ components/The/Footer.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <footer>
-    <div class="py-12">
+  <footer >
+    <div class="py-12 bg-blueGray">
       <div class="container mx-auto">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem repudiandae qui suscipit itaque quae dolorum tempore recusandae autem consequuntur expedita voluptatum eius numquam placeat accusantium, vel quos aliquid inventore aperiam.
       </div>
