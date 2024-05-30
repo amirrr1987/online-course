6c6a19ffcbc7d758dc8a5788f5579b1b7d4b2dd1
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-30 20:12:38 +0330

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="">
     <div class="container mx-auto">
-      <NuxtLink to="/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3"></NuxtLink>
+      <NuxtLink class="bg-green px-12 py-12" to="/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3">Begin Coure</NuxtLink>
     </div>
     
   </section>
