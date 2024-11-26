dacd79a31f8d982fc9d2bd69cb56d8fde8f86320
Amir Maghami <Maghami.A1987@gmail.com>
2024-11-26 22:14:31 +0330

    add

---
+++ client/components/The/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="text-white">
+        <div class="bg-slate-500 py-12">
+            <UContainer>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore asperiores iusto aperiam ex reiciendis, porro obcaecati facere aspernatur odit vero magni dolore explicabo, quibusdam recusandae. Dignissimos aliquam cupiditate sunt quibusdam!
+            </UContainer>
+        </div>
+        <div class="bg-slate-900 py-2">
+            <UContainer>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore asperiores iusto aperiam ex reiciendis, porro obcaecati facere aspernatur odit vero magni dolore explicabo, quibusdam recusandae. Dignissimos aliquam cupiditate sunt quibusdam!
+            </UContainer>
+        </div>
+        
+    </footer>
+</template>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -3,4 +3,5 @@
     <main>
         <slot />
     </main>
+    <TheFooter />
 </template>
