021e55a03a686ca06529a9e3f2119961ff1fac7b
Amir Maghami <Maghami.A1987@gmail.com>
2024-12-01 22:28:49 +0330

    Update users.service.ts

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,26 +1,118 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/user-update-by-id.dto';
+import {
+  DtoUserUpdateByIdRequestBody,
+  DtoUserUpdateByIdRequestParam,
+  DtoUserUpdateByIdResponseBody,
+  UpdateUserDto,
+} from './dto/user-update-by-id.dto';
+import { User as UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IUsersService } from './interfaces/users.service.interface';
+import {
+  DtoUserCreateOneRequestBody,
+  DtoUserCreateOneResponseBody,
+  DtoUserFindAllResponseBody,
+  DtoUserFindByIdRequestParam,
+  DtoUserFindByIdResponseBody,
+  DtoUserDeleteByIdRequestParam,
+  DtoUserDeleteByIdResponseBody,
+} from './dto';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class UsersService implements IUsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  async create(
+    dto: DtoUserCreateOneRequestBody,
+  ): Promise<DtoUserCreateOneResponseBody> {
+    const existingUser = await this.userRepository.findOneBy({
+      value: dto.value,
+    });
+    if (existingUser) {
+      throw new ConflictException(
+        'Role with the provided value already exists.',
+      );
+    }
+    const user = this.userRepository.create(dto);
+    await this.userRepository.save(user);
+    return {
+      succuss: true,
+      message: '',
+      status: 201,
+    };
   }
-
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<DtoUserFindAllResponseBody> {
+    const users = await this.userRepository.find();
+    return {
+      succuss: true,
+      status: 200,
+      message: 'Roles retrieved successfully',
+      data: users,
+    };
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findById(
+    id: DtoUserFindByIdRequestParam,
+  ): Promise<DtoUserFindByIdResponseBody> {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException('Role not found.');
+    }
+    return {
+      succuss: true,
+      message: '',
+      status: 201,
+      data: user,
+    };
   }
+  async updateById(
+    id: DtoUserUpdateByIdRequestParam,
+    dto: DtoUserUpdateByIdRequestBody,
+  ): Promise<DtoUserUpdateByIdResponseBody> {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException('Role not found.');
+    }
+    if (dto.value) {
+      const duplicateRole = await this.userRepository.findOneBy({
+        value: dto.value,
+      });
+      if (duplicateRole && duplicateRole.id !== id) {
+        throw new ConflictException(
+          'Role with the provided value already exists.',
+        );
+      }
+    }
+    const updatedData = {
+      ...dto,
+      updated_at: new Date(),
+    };
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    await this.userRepository.update(id, updatedData);
+    return {
+      succuss: true,
+      message: '',
+      status: 201,
+    };
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async deleteById(
+    id: DtoUserDeleteByIdRequestParam,
+  ): Promise<DtoUserDeleteByIdResponseBody> {
+    const result = await this.userRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException('Role with this ID not found.');
+    }
+    return {
+      succuss: true,
+      message: '',
+      status: 201,
+    };
   }
 }
