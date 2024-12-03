6d822cde7eab31bea95412114f41ce2f86c24a69
Amir Maghami <Maghami.A1987@gmail.com>
2024-12-03 17:20:07 +0330

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
